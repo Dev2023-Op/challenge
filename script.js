f47fa78e23c092a1d5398f1d126a2f08e2a51a0e
Owen Ross <152665582+Dev2023-Op@users.noreply.github.com>
2026-08-25 08:39:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+let userIP = "";
+
+async function fetchAndSaveIP() {
+  try {
+    const response = await fetch('https://ipify.org');
+    const data = await response.json();
+    
+    // 2. Assign the result to your variable
+    userIP = data.ip; 
+    
+    // 3. Any code that depends on the IP must run inside or after this point
+    console.log("Variable updated successfully:", userIP);
+    useIPAddress(); 
+    
+  } catch (error) {
+    console.error("Error grabbing IP:", error);
+  }
+}
+
 const fullUrl = window.location.href; 
 window.addEventListener('DOMContentLoaded', () => {
   fetch('https://ntfy.sh/lewisChallenge', {
     method: 'POST',
-    body: fullUrl,
+    body: fullUrl + " " + userIP,
     headers: {
       'Title': 'lewis is active',
     }
