4099c74cb1217228f621dd458dbd5aadc87dd294
Owen Ross <152665582+Dev2023-Op@users.noreply.github.com>
2026-08-24 14:46:57 +1000

    Fix

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-<script>
-  const fullUrl = window.location.href; 
-  window.addEventListener('DOMContentLoaded', () => {
-    fetch('https://ntfy.sh/lewisChallenge', {
-      method: 'POST',
-      body: fullUrl,
-      headers: {
-        'Title': 'lewis is active',
-        'Tags': 'globe_with_meridians,eyes'
-      }
-    })
-    .then(response => console.log('Notification sent successfully'))
-    .catch(error => console.error('Error sending notification:', error));
-  });
-</script>
+const fullUrl = window.location.href; 
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('https://ntfy.sh/lewisChallenge', {
+    method: 'POST',
+    body: fullUrl,
+    headers: {
+      'Title': 'lewis is active',
+      'Tags': 'globe_with_meridians,eyes'
+    }
+  })
+  .then(response => console.log('Notification sent successfully'))
+  .catch(error => console.error('Error sending notification:', error));
+});
