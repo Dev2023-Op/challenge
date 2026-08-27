13273d9bba38c75aa4b4d6f4f94b3e28e9cb9fee
Owen Ross <152665582+Dev2023-Op@users.noreply.github.com>
2026-08-27 18:59:44 +1000

    remove bloat

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-let userIP = "";
-
-async function fetchAndSaveIP() {
-  try {
-    const response = await fetch('https://ipify.org');
-    const data = await response.json();
-    
-    // 2. Assign the result to your variable
-    userIP = data.ip; 
-    
-    // 3. Any code that depends on the IP must run inside or after this point
-    console.log("Variable updated successfully:", userIP);
-    
-  } catch (error) {
-    console.error("Error grabbing IP:", error);
-  }
-}
-
-let thing;
-
 const fullUrl = window.location.href; 
 window.addEventListener('DOMContentLoaded', () => {
-  thing = (fullUrl + " " + userIP)
   fetch('https://ntfy.sh/lewisChallenge', {
     method: 'POST',
     body: fullUrl + " " + userIP,
